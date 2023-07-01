133b69ad6a9600caa83e34b288cad83ffee49190
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-07-02 00:00:59 +0530

    Time: 1 ms (99.32%), Space: 45.8 MB (43.97%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
@@ -14,35 +14,70 @@
  * }
  */
 class Solution {
+    // more optimized solution
+    HashMap<Integer, List<TreeNode>> map;
+        
+        Solution(){
+            this.map = new HashMap<Integer, List<TreeNode>>();
+            map.put(0, new ArrayList<TreeNode>());
+            List<TreeNode> list = new ArrayList<TreeNode>();
+            list.add(new TreeNode(0));
+            map.put(1, list);
+        }
+    
     public List<TreeNode> allPossibleFBT(int n) {
-        List<TreeNode> result = new ArrayList<>();
+        if(n%2 == 0)
+            return map.get(0);
         
-        if(n%2 == 0){
-            return result;
-        }
+        if(map.containsKey(n))
+            return map.get(n);
         
-        if(n == 1){
-            result.add(new TreeNode(0));
-            return result;
-        }
+        List<TreeNode> answer = new ArrayList<TreeNode>();
         
-        
-        for(int i=1; i<n; i+=2){
+        for(int l=0; l<n; l++){
+            int r = n - 1 -l;
             
-            List<TreeNode> left = allPossibleFBT(i);
-            List<TreeNode> right = allPossibleFBT(n-i-1);
+            List<TreeNode> leftSub = allPossibleFBT(l);
+            List<TreeNode> rightSub = allPossibleFBT(r);
             
-            for(TreeNode l : left){
-                for(TreeNode r : right){
-                    TreeNode root = new TreeNode(0);
-                    root.left = l;
-                    root.right = r;
-                    result.add(root);
+            for(TreeNode tree1 : leftSub){
+                for(TreeNode tree2 : rightSub){
+                    TreeNode tree = new TreeNode(0, tree1, tree2);
+                    answer.add(tree);
                 }
             }
         }
+        map.put(n, answer);
+        return answer;
+               
+//         List<TreeNode> result = new ArrayList<>();
         
-        return result;
+//         if(n%2 == 0){
+//             return result;
+//         }
+        
+//         if(n == 1){
+//             result.add(new TreeNode(0));
+//             return result;
+//         }
+        
+        
+//         for(int i=1; i<n; i+=2){
+            
+//             List<TreeNode> left = allPossibleFBT(i);
+//             List<TreeNode> right = allPossibleFBT(n-i-1);
+            
+//             for(TreeNode l : left){
+//                 for(TreeNode r : right){
+//                     TreeNode root = new TreeNode(0);
+//                     root.left = l;
+//                     root.right = r;
+//                     result.add(root);
+//                 }
+//             }
+//         }
+        
+//         return result;
         
         
     }
