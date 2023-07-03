2c173afe191af368da944a700896c557611d315f
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-07-03 23:29:25 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (89.77%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -15,38 +15,51 @@ class Solution {
         if(list2 == null)
             return list1;
         
-        ListNode head = new ListNode();
-        ListNode key = head;
         
-        while(list2 != null && list1 != null){
-            if(list2.val <= list1.val){
-                key.next = list2;
-                list2 = list2.next;
-            }
-            else{
-                key.next = list1;
-                list1 = list1.next;
-            }
-            key = key.next;
-        }
+        // ************* BEGIN ITERATIVE METHOD *********//
+//       ListNode head = new ListNode();
+//       ListNode key = head;
+//         while(list2 != null && list1 != null){
+//             if(list2.val <= list1.val){
+//                 key.next = list2;
+//                 list2 = list2.next;
+//             }
+//             else{
+//                 key.next = list1;
+//                 list1 = list1.next;
+//             }
+//             key = key.next;
+//         }
         
-        if(list1 != null){
-            while(list1 != null){
-                key.next = list1;
-                key = list1;
-                list1 = list1.next;
-            }
+//         if(list1 != null){
+//             while(list1 != null){
+//                 key.next = list1;
+//                 key = list1;
+//                 list1 = list1.next;
+//             }
+//         }
+//         else{
+//             while(list2 != null){
+//                 key.next = list2;
+//                 key = list2;
+//                 list2 = list2.next;
+//             }
+//         }
+        
+//         return head.next; 
+        // ************* END OF ITERATIVE METHOD *********//
+        
+        
+        // ** RECURSIVE SOLUTION** //
+        if(list2.val <= list1.val){
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
         else{
-            while(list2 != null){
-                key.next = list2;
-                key = list2;
-                list2 = list2.next;
-            }
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         }
-        
-        return head.next; 
-        
+
         
     }
 }
