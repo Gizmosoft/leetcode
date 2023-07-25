0c25851054b71769944198e91c854ef1fb71cb9a
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-07-25 18:11:09 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (73.13%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,28 +1,27 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] visited = new boolean[image.length][image[0].length];
+        // boolean[][] visited = new boolean[image.length][image[0].length];
         
         int target = image[sr][sc];
         
-        int[][] result = helper(image, sr, sc, color, visited, target);
+        if(image[sr][sc] == color)
+            return image;
+        
+        int[][] result = helper(image, sr, sc, color, target);
         
         return result;
     }
     
-    public int[][] helper(int[][] image, int sr, int sc, int color, boolean[][] visited, int target){
-        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || visited[sr][sc] || image[sr][sc] != target)
+    public int[][] helper(int[][] image, int sr, int sc, int color, int target){
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] != target)
             return image;
         
-        if(image[sr][sc] == target){
-            image[sr][sc] = color;
-        }
+        image[sr][sc] = color;
         
-        visited[sr][sc] = true;
-        helper(image, sr-1, sc, color, visited, target); // top
-        helper(image, sr+1, sc, color, visited, target); // bootom
-        helper(image, sr, sc-1, color, visited, target); // left
-        helper(image, sr, sc+1, color, visited, target); // right
-        visited[sr][sc] = false;
+        helper(image, sr-1, sc, color, target); // top
+        helper(image, sr+1, sc, color, target); // bootom
+        helper(image, sr, sc-1, color, target); // left
+        helper(image, sr, sc+1, color, target); // right
         
         return image;
     }
