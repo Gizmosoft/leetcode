fc8aca969b94fc79ce5b27414f87808d06f2026f
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-07-26 17:38:13 +0530

    Time: 22 ms (72.46%), Space: 44.4 MB (33.81%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -6,7 +6,9 @@ class Solution {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         
         for(int i=0; i<nums.length-3; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
             for(int j=i+1; j<nums.length-2; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
                 int k = j+1;
                 int l = nums.length-1;
                 
