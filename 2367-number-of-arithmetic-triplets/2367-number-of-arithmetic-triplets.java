b0d69d0878f21cdc336e47a9f3c3877ea6fbe7b9
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-07-30 19:56:36 +0530

    Time: 1 ms (96.33%), Space: 40 MB (96.06%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -2,23 +2,39 @@ import java.util.*;
 
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
-        if(nums.length < 3){
-            return 0;
-        }
-        Set<String> set = new HashSet<>();
+        // Memoized approach
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
         
-        for(int i=0; i<nums.length-1; i++){
-            for(int j=i+1; j<nums.length; j++){
-                int k = nums.length-1;
-                while(k>j){
-                    if(nums[k]-nums[j] == diff && nums[j]-nums[i] == diff){
-                        set.add(Integer.toString(i) + "," + Integer.toString(j) + "," + Integer.toString(k));
-                    }
-                    k--;
-                }
-            }
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
         }
         
-        return set.size();
+        for(int i=0; i<set.size(); i++){
+            if(set.contains(nums[i]-diff) && set.contains(nums[i]+diff))
+                count++;
+        }
+        
+        return count;
+        
+        // Slower Approach
+//         if(nums.length < 3){
+//             return 0;
+//         }
+//         Set<String> set = new HashSet<>();
+        
+//         for(int i=0; i<nums.length-1; i++){
+//             for(int j=i+1; j<nums.length; j++){
+//                 int k = nums.length-1;
+//                 while(k>j){
+//                     if(nums[k]-nums[j] == diff && nums[j]-nums[i] == diff){
+//                         set.add(Integer.toString(i) + "," + Integer.toString(j) + "," + Integer.toString(k));
+//                     }
+//                     k--;
+//                 }
+//             }
+//         }
+        
+//         return set.size();
     }
 }
