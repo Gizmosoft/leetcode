98c057f2437ac302422cf384fec053cb4d1aae9a
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-08-01 20:40:04 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (86.91%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,30 +1,50 @@
 class Solution {
     public int balancedStringSplit(String s) {
-        if(s.length()%2 != 0)
-            return 0;
-        
-        Stack<Character> stack = new Stack<>();
-        int count = 0;
+//         FASTER APPROACH 0ms
+        int l = 0, r = 0, ans = 0;
         
         for(int i=0; i<s.length(); i++){
-            if(stack.isEmpty()){
-                count++;
-                stack.push(s.charAt(i));
+            char c = s.charAt(i);
+            if(c == 'R')
+                r++;
+            else
+                l++;
+            
+            if(l == r){
+                ans++;
+                l = 0;
+                r = 0;
             }
-            else{
-                // make a choice, either to push to the stack or pop an existing element out            
-                if(s.charAt(i) == stack.peek()){
-                    stack.push(s.charAt(i));
-                }
-                else{
-                    stack.pop();
-                }
-            }            
         }
         
-        if(!stack.isEmpty())
-            return count-1;
+        return ans;
         
-        return count;        
+//         SLOWER APPROACH
+//         if(s.length()%2 != 0)
+//             return 0;
+        
+//         Stack<Character> stack = new Stack<>();
+//         int count = 0;
+        
+//         for(int i=0; i<s.length(); i++){
+//             if(stack.isEmpty()){
+//                 count++;
+//                 stack.push(s.charAt(i));
+//             }
+//             else{
+//                 // make a choice, either to push to the stack or pop an existing element out            
+//                 if(s.charAt(i) == stack.peek()){
+//                     stack.push(s.charAt(i));
+//                 }
+//                 else{
+//                     stack.pop();
+//                 }
+//             }            
+//         }
+        
+//         if(!stack.isEmpty())
+//             return count-1;
+        
+//         return count;        
     }
 }
