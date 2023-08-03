e7123d1029dffbf673bb9f93275182f08af53b93
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-08-03 22:58:32 +0530

    Time: 1 ms (61.40%), Space: 39.1 MB (88.48%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int maximum69Number (int num) { 
+        char[] numArr = String.valueOf(num).toCharArray();
+        
+        for(int i=0; i<numArr.length; i++){
+            if(numArr[i] == '6'){
+                numArr[i] = '9';
+                return Integer.valueOf(String.valueOf(numArr));
+            }
+        }
+        return num;
+        
+    }
+}
