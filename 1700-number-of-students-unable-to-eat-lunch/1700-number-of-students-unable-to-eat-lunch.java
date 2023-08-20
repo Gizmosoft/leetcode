b224c90650a9fc6ed92a551ba4606bdb30c34b93
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-08-20 13:32:07 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (67.48%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,31 +1,58 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        Stack<Integer> sandwich = new Stack<>();
-        Queue<Integer> student = new LinkedList<>();
-        
-        int n = students.length;
-        
-        for(int i=0; i<n; i++){
-            student.offer(students[i]);
-            sandwich.push(sandwiches[n-i-1]);
+//         Runtime 0ms
+        int zeros = 0, ones = 0;
+        for(int s : students){
+            if(s == 0)
+                zeros++;
+            else
+                ones++;
         }
         
-        while(student.size() != 0){
-            if(student.peek() == sandwich.peek()){
-                sandwich.pop();
-                student.poll();
+        for(int f : sandwiches){
+            if(f == 0){
+                if(zeros == 0){
+                    return ones;
+                }
+                zeros--;
             }
             else{
-                if(student.contains(sandwich.peek())){
-                    student.offer(student.poll());
+                if(ones == 0){
+                    return zeros;
                 }
-                else{
-                    return student.size();
-                }
+                ones--;
             }
         }
         
-        return student.size();
+        return 0;
+        
+//         Runtime 2ms        
+//         Stack<Integer> sandwich = new Stack<>();
+//         Queue<Integer> student = new LinkedList<>();
+        
+//         int n = students.length;
+        
+//         for(int i=0; i<n; i++){
+//             student.offer(students[i]);
+//             sandwich.push(sandwiches[n-i-1]);
+//         }
+        
+//         while(student.size() != 0){
+//             if(student.peek() == sandwich.peek()){
+//                 sandwich.pop();
+//                 student.poll();
+//             }
+//             else{
+//                 if(student.contains(sandwich.peek())){
+//                     student.offer(student.poll());
+//                 }
+//                 else{
+//                     return student.size();
+//                 }
+//             }
+//         }
+        
+//         return student.size();
         
     }
 }
