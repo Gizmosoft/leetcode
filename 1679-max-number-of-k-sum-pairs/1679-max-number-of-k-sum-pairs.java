25f3be4ffe851513dcb963d7b63a7d5b893ddfb4
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-09-21 20:43:28 -0400

    Time: 18 ms (95.71%), Space: 55.6 MB (34.99%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int count = 0;
+        int i = 0, j = nums.length-1;
+        while(i<j){
+            if(nums[i] + nums[j] == k){
+                count += 1;
+                i++;
+                j--;
+            }
+            else if(nums[i] + nums[j] < k){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        
+        
+        return count;
+        
+    }
+}
