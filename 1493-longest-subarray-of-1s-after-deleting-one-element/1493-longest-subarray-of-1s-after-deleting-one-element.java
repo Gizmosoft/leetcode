eb16c4b47b23029a3c0e97eebea7bfe0f43fbed5
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-09-30 21:28:08 -0400

    Time: 3 ms (43.83%), Space: 55.9 MB (19.57%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int left = 0, right = 0;
+        int flag = 1;
+        int n = nums.length;
+        int max = 0;
+        
+        while(right < n){
+            if(nums[right] == 0){
+                flag--;
+            }
+            
+            if(flag < 0){
+                if(nums[left] == 0){
+                    flag++;
+                }
+                left++;
+            }
+            max = Math.max(max, right-left);
+            right++;
+        }
+        
+        return max;
+    }
+}
