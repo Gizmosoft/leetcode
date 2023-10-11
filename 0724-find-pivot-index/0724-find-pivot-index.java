402c515a3c222ffaa2e03b7c81ff20611b1b250c
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-10-11 13:32:15 -0400

    Time: 1 ms (98.88%), Space: 44.2 MB (43.45%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,24 +1,41 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         
+        // calculate full sum
+        int sum = 0;
         for(int i=0; i<nums.length; i++){
-            int left = 0, right = 0;
-            // calculate left sum
-            for(int j=0; j<i; j++){
-                left += nums[j];
-            }
-            
-            // calculate right sum
-            for(int j=i+1; j<nums.length; j++){
-                right += nums[j];
-            }
-            
-            // check if condition satisfies
+            sum += nums[i];
+        }
+        
+        int prevLeft = 0;
+        for(int i=0; i<nums.length; i++){
+            int right = sum - nums[i] - prevLeft;
+            int left = sum - nums[i] - right;
+            prevLeft += nums[i];
             if(left == right)
                 return i;
         }
         
         return -1;
+        
+//         for(int i=0; i<nums.length; i++){
+//             int left = 0, right = 0;
+//             // calculate left sum
+//             for(int j=0; j<i; j++){
+//                 left += nums[j];
+//             }
+            
+//             // calculate right sum
+//             for(int j=i+1; j<nums.length; j++){
+//                 right += nums[j];
+//             }
+            
+//             // check if condition satisfies
+//             if(left == right)
+//                 return i;
+//         }
+        
+//         return -1;
         
     }
 }
