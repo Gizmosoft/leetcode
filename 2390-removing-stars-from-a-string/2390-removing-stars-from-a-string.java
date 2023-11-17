182466b6c839372b9d97a030d95750fbac3feedb
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-11-16 21:21:06 -0500

    Time: 12 ms (98.47%), Space: 45.2 MB (53.00%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,21 +1,34 @@
 class Solution {
     public String removeStars(String s) {
-        Stack<Character> stack = new Stack<>();
-        String result = "";
+        char[] result = s.toCharArray();
+        int flag = 0;
         
         for(int i=0; i<s.length(); i++) {
-            char ch = s.charAt(i);
-            if(ch == '*') {
-                stack.pop();
-            } else {
-                stack.push(ch);
+            if(result[i] != '*') {
+                result[flag] = result[i];
+                flag++;
+            } else if(flag > 0 && result[flag-1] != '*') {
+                flag--;
             }
         }
         
-        for(Character c : stack) {
-            result += c;
-        }
+        return new String(result, 0, flag);
+//         Stack<Character> stack = new Stack<>();
+//         String result = "";
         
-        return result;
+//         for(int i=0; i<s.length(); i++) {
+//             char ch = s.charAt(i);
+//             if(ch == '*') {
+//                 stack.pop();
+//             } else {
+//                 stack.push(ch);
+//             }
+//         }
+        
+//         for(Character c : stack) {
+//             result += c;
+//         }
+        
+//         return result;
     }
 }
