4531009a32ee24d1298a1b9d5091b58da9de84b6
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2023-12-25 18:37:28 -0500

    Time: 0 ms (100.00%), Space: 41.5 MB (13.06%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,32 +1,40 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String res = "";
-        
-        // get the length of the smallest word in the array
-        int lowLen = getLenOfSmallest(strs);
-        int i=0;
-        while(i < lowLen) {
-            Character c = Character.MIN_VALUE;
-            for(String s : strs) {
-                if(c == Character.MIN_VALUE || c == s.charAt(i)) {
-                    c = s.charAt(i);
-                } else {
-                    return res;
-                }
-            }
-            res += c.toString();
-            i++;
-        }
-        
-        return res;
+        String prefix = strs[0];
+
+        for (String s : strs)
+            while (!s.startsWith(prefix))
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+        return prefix;
     }
+//         String res = "";
+        
+//         // get the length of the smallest word in the array
+//         int lowLen = getLenOfSmallest(strs);
+//         int i=0;
+//         while(i < lowLen) {
+//             Character c = Character.MIN_VALUE;
+//             for(String s : strs) {
+//                 if(c == Character.MIN_VALUE || c == s.charAt(i)) {
+//                     c = s.charAt(i);
+//                 } else {
+//                     return res;
+//                 }
+//             }
+//             res += c.toString();
+//             i++;
+//         }
+        
+//         return res;
+//     }
     
-    public static int getLenOfSmallest(String[] strs) {
-        int len = Integer.MAX_VALUE;
-        for(String s : strs) {
-            len = Math.min(len, s.length());
-            // System.out.println(len);
-        }
-        return len;
-    }
+//     public static int getLenOfSmallest(String[] strs) {
+//         int len = Integer.MAX_VALUE;
+//         for(String s : strs) {
+//             len = Math.min(len, s.length());
+//             // System.out.println(len);
+//         }
+//         return len;
+//     }
 }
