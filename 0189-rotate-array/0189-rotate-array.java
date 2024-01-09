734486e1c64d8aad1799d1d4f955092e5f2b2b7c
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2024-01-09 18:38:59 -0500

    Time: 0 ms (100.00%), Space: 57.5 MB (12.70%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,16 +1,37 @@
 class Solution {
+    // O(n) solution with space complexity of O(1)
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        
-        int[] ans = new int[n];
-        
-        for(int i=0; i<n; i++) {
-            ans[(i+k)%n] = nums[i];            
-        }
-        
-        for(int i=0; i<n; i++) {
-            nums[i] = ans[i];
-        }
-        
+        k = k%n;
+        reverseArr(nums, 0, n-1);   // first reverse the full array
+        reverseArr(nums, 0, k-1);   // then reverse the first k elements
+        reverseArr(nums, k, n-1);   // then reverse the remaining elements        
     }
+    
+    public void reverseArr(int[] nums, int start, int end) {
+        
+        while(start < end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+//     O(n) Time solution with O(n) space
+//     public void rotate(int[] nums, int k) {
+//         int n = nums.length;
+        
+//         int[] ans = new int[n];
+        
+//         for(int i=0; i<n; i++) {
+//             ans[(i+k)%n] = nums[i];            
+//         }
+        
+//         for(int i=0; i<n; i++) {
+//             nums[i] = ans[i];
+//         }
+        
+//     }
 }
