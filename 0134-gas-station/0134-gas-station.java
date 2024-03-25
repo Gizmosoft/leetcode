e9e5c146d49a519afacf782615d1be02912d2fa7
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2024-03-25 12:03:55 -0400

    Time: 3 ms (47.44%), Space: 57 MB (47.55%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,47 +1,23 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalSurplus = 0;
-        int currentSurplus = 0;
+        int total = 0;
+        int current = 0;
         int startStation = 0;
         int n = gas.length;
 
         for (int i = 0; i < n; i++) {
-            totalSurplus += gas[i%n] - cost[i%n];
-            currentSurplus += gas[i%n] - cost[i%n];
+            total += gas[i] - cost[i];
+            current += gas[i] - cost[i];
 
             // If surplus is negative, you can't reach station i+1 from the current starting point
-            if (currentSurplus < 0) {
+            if (current < 0) {
                 // Try the next station as the starting point
-                startStation = (i + 1)%n;
-                currentSurplus = 0; // Reset the current surplus
+                startStation = i+1;
+                current = 0; // Reset the current surplus
             }
         }
 
         // Check if the total amount of gas is less than the total travel cost
-        return totalSurplus >= 0 ? startStation : -1;
-        
-        
-//         int n = gas.length;
-        
-//         for(int i=0; i<n; i++) {
-//             int iters = n;
-//             int tank = gas[i];
-//             int index = i;
-//             int next;
-//             while(iters > 0) {
-//                 next = (index+1)%n;
-//                 tank = tank - cost[index] + gas[next];
-//                 if(tank < cost[next]) {
-//                     break;
-//                 }                
-//                 iters--;
-//                 index = next;
-//             }
-            
-//             if(iters == 0) {
-//                 return i;
-//             }
-//         }
-//         return -1;
+        return total >= 0 ? startStation : -1;
     }
 }
