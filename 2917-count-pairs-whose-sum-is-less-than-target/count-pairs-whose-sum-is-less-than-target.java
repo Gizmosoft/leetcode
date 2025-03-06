1d632cadcb2b6f17128d1c589d3afbec2e116b85
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-03-05 22:04:55 -0500

    Time: 3 ms (38.96%) | Memory: 42.7 MB (29.77%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
@@ -3,16 +3,28 @@ class Solution {
         Collections.sort(nums);
         int n = nums.size();
         int pairs = 0;
+        int right = n-1;
+        int left = 0;
 
-        for(int i=0; i<n; i++) {
-            int right = n-1;
-            while(right > i) {
-                if(nums.get(i) + nums.get(right) < target){
-                    pairs++;
-                }
+        // Optimization
+        while(left < right) {
+            if(nums.get(left) + nums.get(right) < target) {
+                pairs += right - left;  // if the pair having largest gap b/w them is lesser than target then all pairs in b/w will also sum up to be smaller than target
+                left++; // move to the next left pointer as all pairs for the current have been added to solution
+            } else {
                 right--;
             }
         }
+
+        // for(int i=0; i<n; i++) {
+        //     int right = n-1;
+        //     while(right > i) {
+        //         if(nums.get(i) + nums.get(right) < target){
+        //             pairs++;
+        //         }
+        //         right--;
+        //     }
+        // }
         return pairs;
     }
 }
