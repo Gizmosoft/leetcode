a8c7d4d37774ee415843c1141ff530b2c0468d8e
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-03-29 16:34:24 -0400

    Time: 1 ms (70.56%) | Memory: 42.3 MB (42.80%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,27 +15,57 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        // One Queue + getting rightmost element
         if(root == null) return new ArrayList<Integer>();
 
-        Queue<TreeNode> next = new LinkedList<>();
-        next.offer(root);
-        Queue<TreeNode> curr = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
         List<Integer> res = new ArrayList<>();
 
-        TreeNode node = null;
-        while(!next.isEmpty()) {
-            curr = next;
-            next = new LinkedList<>();
+        while(!queue.isEmpty()) {
+            int level = queue.size();
 
-            while(!curr.isEmpty()) {
-                node = curr.poll();
+            for(int i=0; i<level; i++) {
+                TreeNode node = queue.poll();
 
-                if(node.left != null) next.offer(node.left);
-                if(node.right != null) next.offer(node.right);
+                if(i == level-1) {
+                    res.add(node.val);
+                }
+
+                if(node.left != null) {
+                    queue.offer(node.left);
+                }
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
             }
-            res.add(node.val);
         }
 
-        return res;        
+        return res;
+
+        // BFS using 2 Queues
+        // if(root == null) return new ArrayList<Integer>();
+
+        // Queue<TreeNode> next = new LinkedList<>();
+        // next.offer(root);
+        // Queue<TreeNode> curr = new LinkedList<>();
+        // List<Integer> res = new ArrayList<>();
+
+        // TreeNode node = null;
+        // while(!next.isEmpty()) {
+        //     curr = next;
+        //     next = new LinkedList<>();
+
+        //     while(!curr.isEmpty()) {
+        //         node = curr.poll();
+
+        //         if(node.left != null) next.offer(node.left);
+        //         if(node.right != null) next.offer(node.right);
+        //     }
+        //     res.add(node.val);
+        // }
+
+        // return res;        
     }
 }
