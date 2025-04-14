c162374758537f446452364c079ee035ce7b3149
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-04-14 11:01:51 -0400

    Time: 15 ms (41.43%) | Memory: 45.8 MB (11.71%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,27 +1,46 @@
 class Solution {
     public int countSubstrings(String s) {
-        int res = 0;
+        // DYNAMIC PROGRAMMING APPROACHING
+        int n = s.length(), res = 0;
+        boolean[][] tracker = new boolean[n][n];
 
-        for(int i=0; i<s.length(); i++) {
-            int l = i, r = i;
-            res += countPalindromes(s, l, r);
-
-            l = i;
-            r = i+1;
-            res += countPalindromes(s, l, r);
+        for(int i=n-1; i>=0; i--) {
+            for(int j=i; j<n; j++) {
+                if(s.charAt(i) == s.charAt(j)) {
+                    if(j-i <= 1 || tracker[i+1][j-1]) {
+                        tracker[i][j] = true;
+                        res++;
+                    }
+                }
+            }
         }
 
         return res;
+
+
+        // TWO POINTER APPROACH
+        // int res = 0;
+
+        // for(int i=0; i<s.length(); i++) {
+        //     int l = i, r = i;
+        //     res += countPalindromes(s, l, r);
+
+        //     l = i;
+        //     r = i+1;
+        //     res += countPalindromes(s, l, r);
+        // }
+
+        // return res;
     }
 
-    private int countPalindromes(String s, int l, int r) {
-        int res = 0;
+//     private int countPalindromes(String s, int l, int r) {
+//         int res = 0;
 
-        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-            res += 1;
-            l -= 1;
-            r += 1;
-        }
-        return res;
-    }
+//         while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+//             res += 1;
+//             l -= 1;
+//             r += 1;
+//         }
+//         return res;
+//     }
 }
