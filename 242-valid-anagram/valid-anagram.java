3508b5e722995f3df107cf9d784ae1a5c8f5b647
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-04-16 22:32:26 -0400

    Time: 5 ms (53.94%) | Memory: 43 MB (90.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,20 +1,35 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        int[] sCount = new int[26];
-        int[] tCount = new int[26];
 
+        int[] counter = new int[26];
         for(int i=0; i<s.length(); i++) {
-            int sIndex = s.charAt(i) - 'a';
-            sCount[sIndex]++;
-            int tIndex = t.charAt(i) - 'a';
-            tCount[tIndex]++;
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
         }
 
-        String sString = Arrays.toString(sCount);
-        String tString = Arrays.toString(tCount);
+        for(int count : counter) {
+            if(count != 0) {
+                return false;
+            }
+        }
+        return true;
 
-        if(sString.equals(tString)) return true;
-        return false;
+        // if(s.length() != t.length()) return false;
+        // int[] sCount = new int[26];
+        // int[] tCount = new int[26];
+
+        // for(int i=0; i<s.length(); i++) {
+        //     int sIndex = s.charAt(i) - 'a';
+        //     sCount[sIndex]++;
+        //     int tIndex = t.charAt(i) - 'a';
+        //     tCount[tIndex]++;
+        // }
+
+        // String sString = Arrays.toString(sCount);
+        // String tString = Arrays.toString(tCount);
+
+        // if(sString.equals(tString)) return true;
+        // return false;
     }
 }
