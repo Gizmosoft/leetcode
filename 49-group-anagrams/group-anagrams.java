8e6e9cc3fb99667e06c6a638b9e0c60b7a314d32
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-04-16 21:45:17 -0400

    Time: 19 ms (20.49%) | Memory: 48.6 MB (19.84%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,17 +1,36 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // USING HASHMAP
+        // USING COUNT CATEGORIZATION AND HASHMAP - O(nk)
         if(strs.length == 0) return new ArrayList<>();
-
+        int[] count = new int[26];
         Map<String, List<String>> ans = new HashMap<>();
         for(String s : strs) {
-            char[] ch = s.toCharArray();
-            Arrays.sort(ch);
-            String key = String.valueOf(ch);
-            if(!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+            Arrays.fill(count, 0);
+            for(char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for(int i=0; i<26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if(!ans.containsKey(key)) ans.put(key, new ArrayList());
             ans.get(key).add(s);
         }
         return new ArrayList(ans.values());
+
+        // USING HASHMAP - O(n*klogk)
+        // if(strs.length == 0) return new ArrayList<>();
+
+        // Map<String, List<String>> ans = new HashMap<>();
+        // for(String s : strs) {
+        //     char[] ch = s.toCharArray();
+        //     Arrays.sort(ch);
+        //     String key = String.valueOf(ch);
+        //     if(!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+        //     ans.get(key).add(s);
+        // }
+        // return new ArrayList(ans.values());
 
         // BRUTE FORCE
         // List<List<String>> ans = new ArrayList<>();
