40e2a684e87cb64dbb841f0cec912b05fca71d7d
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-04-27 20:26:49 -0400

    Time: 13 ms (86.82%) | Memory: 45.4 MB (73.63%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,37 +1,65 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        int start = 0;
-        int end = s.length()-1;
-        String result = "";
+        StringBuilder sb = new StringBuilder("");
+        int open = 0;
 
-        char[] arr = s.toCharArray();
-
-        int openingCount = 0;
-
-        // Parse from left to right
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] == '(')
-                openingCount++;
-            else if (arr[i] == ')') {
-                if (openingCount == 0)
-                    arr[i] = '*';
-                else
-                    openingCount--;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == ')' && open > 0) {    // append closing parantheses only when there is something opened already
+                sb.append(s.charAt(i));
+                open--;
+            } else if(s.charAt(i) == '(') {         // append opening parantheses and increment count
+                sb.append(s.charAt(i));
+                open++;
+            } else if(s.charAt(i) != ')') {         // append normal alphabets
+                sb.append(s.charAt(i));     
             }
         }
 
-        // Parse from right to left
-        for(int i=arr.length-1; i>=0; i--) {
-            if(openingCount > 0 && arr[i] == '(') {
-                arr[i] = '*';
-                openingCount--;
+        if(open > 0) {
+            for(int i=sb.length()-1; i>=0; i--) {
+                if(open == 0) break;
+                if(sb.charAt(i) == '(') {
+                    sb.deleteCharAt(i);
+                    open--;
+                }    
             }
         }
 
-        for(char c: arr) {
-            if(c != '*')
-                result += c;
-        }
-        return result;
+        return sb.toString();
+
+        // SLOWER APPROACH - 2 parse
+        // int start = 0;
+        // int end = s.length()-1;
+        // String result = "";
+
+        // char[] arr = s.toCharArray();
+
+        // int openingCount = 0;
+
+        // // Parse from left to right
+        // for(int i=0; i<arr.length; i++) {
+        //     if(arr[i] == '(')
+        //         openingCount++;
+        //     else if (arr[i] == ')') {
+        //         if (openingCount == 0)
+        //             arr[i] = '*';
+        //         else
+        //             openingCount--;
+        //     }
+        // }
+
+        // // Parse from right to left
+        // for(int i=arr.length-1; i>=0; i--) {
+        //     if(openingCount > 0 && arr[i] == '(') {
+        //         arr[i] = '*';
+        //         openingCount--;
+        //     }
+        // }
+
+        // for(char c: arr) {
+        //     if(c != '*')
+        //         result += c;
+        // }
+        // return result;
     }
 }
