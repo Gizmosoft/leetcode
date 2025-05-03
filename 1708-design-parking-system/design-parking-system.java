1940f7251dab8497a2ddca30223f4e4c78ad8e82
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-05-03 15:44:19 -0400

    Time: 8 ms (68.56%) | Memory: 45.3 MB (70.44%) - LeetSync

--- 1708-design-parking-system/design-parking-system.java
+++ 1708-design-parking-system/design-parking-system.java
@@ -1,41 +1,48 @@
 class ParkingSystem {
-    private int big;
-    private int medium;
-    private int small;
+    // private int big;
+    // private int medium;
+    // private int small;
 
+    private int[] spotsAvailable;
     public ParkingSystem(int big, int medium, int small) {
-        this.big = big;
-        this.medium = medium;
-        this.small = small;
+        // this.big = big;
+        // this.medium = medium;
+        // this.small = small;
+        spotsAvailable = new int[]{0, big, medium, small};
     }
     
     public boolean addCar(int carType) {
-        switch (carType) {
-            case 1:
-                if(this.big >= 1) {
-                    this.big -= 1;
-                    return true;
-                } else
-                    return false;
-            
-            case 2:
-                if(this.medium >= 1) {
-                    this.medium -= 1;
-                    return true;
-                } else
-                    return false;
-            
-            case 3:
-            if (this.small >= 1) {
-                this.small -= 1;
-                return true;
-            } else {
-                return false;
-            }
+        if(spotsAvailable[carType] == 0) return false;
 
-            default:
-                return false; 
-        }
+        --spotsAvailable[carType];
+        return true;
+
+        // switch (carType) {
+        //     case 1:
+        //         if(this.big >= 1) {
+        //             this.big -= 1;
+        //             return true;
+        //         } else
+        //             return false;
+            
+        //     case 2:
+        //         if(this.medium >= 1) {
+        //             this.medium -= 1;
+        //             return true;
+        //         } else
+        //             return false;
+            
+        //     case 3:
+        //     if (this.small >= 1) {
+        //         this.small -= 1;
+        //         return true;
+        //     } else {
+        //         return false;
+        //     }
+
+        //     default:
+        //         return false; 
+        // }
     }
 }
 
