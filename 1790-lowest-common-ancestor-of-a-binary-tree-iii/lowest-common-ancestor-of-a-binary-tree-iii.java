e9e537edee82e6661c98c8986985f369e34f26ac
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-05-14 22:45:08 -0400

    Time: 30 ms (28.91%) | Memory: 44.7 MB (46.33%) - LeetSync

---
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -0,0 +1,29 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Set<Node> path = findPath(p);
+        while (q.parent != null) {
+            if(path.contains(q)) return q;
+            q = q.parent;
+        }
+        return q;
+    }
+
+    public Set<Node> findPath(Node p) {
+        Set<Node> path = new HashSet<>();
+        while(p.parent != null) {
+            path.add(p);
+            p = p.parent;
+        }
+        return path;
+    }
+}
