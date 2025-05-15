f8fd2f31f60a188ad8bfb2b539c3d04eea5b51d0
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-05-14 22:51:44 -0400

    Time: 27 ms (99.28%) | Memory: 44.2 MB (99.06%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -10,20 +10,29 @@ class Node {
 
 class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
-        Set<Node> path = findPath(p);
-        while (q.parent != null) {
-            if(path.contains(q)) return q;
-            q = q.parent;
+        // Constant Space solution
+        Node a = p, b = q;
+        while(a != b) {
+            a = a == null ? q : a.parent;
+            b = b == null ? p : b.parent;
         }
-        return q;
+        return a;
+
+        // O(n^2) time and O(n) space solution
+        // Set<Node> path = findPath(p);
+        // while (q.parent != null) {
+        //     if(path.contains(q)) return q;
+        //     q = q.parent;
+        // }
+        // return q;
     }
 
-    public Set<Node> findPath(Node p) {
-        Set<Node> path = new HashSet<>();
-        while(p.parent != null) {
-            path.add(p);
-            p = p.parent;
-        }
-        return path;
-    }
+    // public Set<Node> findPath(Node p) {
+    //     Set<Node> path = new HashSet<>();
+    //     while(p.parent != null) {
+    //         path.add(p);
+    //         p = p.parent;
+    //     }
+    //     return path;
+    // }
 }
