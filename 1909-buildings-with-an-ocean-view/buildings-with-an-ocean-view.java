ff5e46545709355f1fe81691f3ab161a0c9000e8
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-05-17 12:02:31 -0400

    Time: 18 ms (31.48%) | Memory: 59.8 MB (63.39%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
@@ -1,20 +1,40 @@
 class Solution {
     public int[] findBuildings(int[] heights) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int max = 0;
-        for(int i=heights.length-1; i>=0; i--) {
-            if(heights[i] > max) {
-                max = heights[i];
-                pq.add(i);
+        // O(n) time and O(n) space
+        int n = heights.length;
+        List<Integer> list = new ArrayList<>();     // stores index of allowed buildings
+
+        for(int i=0; i<n; i++) {
+            // if current building taller than the last added building to list, remove all previous elements in list
+            while(!list.isEmpty() && heights[list.get(list.size()-1)] <= heights[i]) {
+                list.remove(list.size() - 1);
             }
+            list.add(i);
         }
 
-        int[] ans = new int[pq.size()];
-        int i = 0;
-        while(!pq.isEmpty()) {
-            ans[i++] = pq.poll();
+        // transfer list indexes to array
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
         }
+        return answer;
+        
+        // O(nlogn) Time & O(n) space
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // int max = 0;
+        // for(int i=heights.length-1; i>=0; i--) {
+        //     if(heights[i] > max) {
+        //         max = heights[i];
+        //         pq.add(i);
+        //     }
+        // }
 
-        return ans;
+        // int[] ans = new int[pq.size()];
+        // int i = 0;
+        // while(!pq.isEmpty()) {
+        //     ans[i++] = pq.poll();
+        // }
+
+        // return ans;
     }
 }
