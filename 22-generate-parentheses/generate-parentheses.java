1ee3912bc32ad8ad7f9898297761f2f9dfa19939
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-05-22 11:21:19 -0400

    Time: 1 ms (82.23%) | Memory: 43 MB (82.84%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> answer = new ArrayList<>();
+        backtrack(n, 0, 0, new StringBuilder(), answer);
+        return answer;
+    }
+
+    public void backtrack(int n, int open, int closed, StringBuilder s, List<String> answer) {
+        // if(closed < open || open > n || closed > n) return;
+
+        if(open == n && closed == n) {
+            answer.add(s.toString());
+            return;
+        }
+
+        if(open < n) {
+            s.append("(");
+            backtrack(n, open+1, closed, s, answer);
+            s.deleteCharAt(s.length()-1);
+        }
+
+        if(closed < open) {
+            s.append(")");
+            backtrack(n, open, closed+1, s, answer);
+            s.deleteCharAt(s.length()-1);
+        }
+    }
+}
