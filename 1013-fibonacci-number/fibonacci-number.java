913fd7a81aa62ca18ef17fee62d85965ed2ae3f3
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-02 15:21:34 -0400

    Time: 0 ms (100.00%) | Memory: 40.1 MB (90.47%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int fib(int n) {
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
+        // Arrays.fill(dp, -1);     // Not required for tabulation approach
         if(n == 0) return 0;
         if(n == 1) return 1;
         return helper(n, dp);
