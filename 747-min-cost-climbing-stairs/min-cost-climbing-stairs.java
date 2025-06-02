1ab93815e2f8273f3f2362bb9497aead364ec6e9
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-02 18:28:19 -0400

    Time: 0 ms (100.00%) | Memory: 43 MB (77.97%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -5,9 +5,14 @@ class Solution {
         // return Math.min(recursive(cost, n-1), recursive(cost, n-2));
 
         // DP Solution
+        // int[] dp = new int[n];
+        // Arrays.fill(dp, -1);     // Only needed for DP
+        // return Math.min(recursiveDP(cost, n-1, dp), recursiveDP(cost, n-2, dp));
+
+        // DP with Tabulation
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return Math.min(recursiveDP(cost, n-1, dp), recursiveDP(cost, n-2, dp));
+        return dpTabulation(cost, dp, n);
+
     }
 
     // Simple Recursion O(2^n)
@@ -18,12 +23,25 @@ class Solution {
     //     return cost[i] + Math.min(recursive(cost, i-1), recursive(cost, i-2));
     // }
 
-    public int recursiveDP(int[] cost, int i, int[] dp) {
-        if(i < 0) return 0;
-        if(i == 0 || i == 1) return cost[i];
+    // Dynamic Programming O(n)
+    // public int recursiveDP(int[] cost, int i, int[] dp) {
+    //     if(i < 0) return 0;
+    //     if(i == 0 || i == 1) return cost[i];
 
-        if(dp[i] != -1) return dp[i];
-        dp[i] = cost[i] + Math.min(recursiveDP(cost, i-1, dp), recursiveDP(cost, i-2, dp));
-        return dp[i];
+    //     if(dp[i] != -1) return dp[i];
+    //     dp[i] = cost[i] + Math.min(recursiveDP(cost, i-1, dp), recursiveDP(cost, i-2, dp));
+    //     return dp[i];
+    // }
+
+    // Dynamic Programming with Tabulation
+    public int dpTabulation(int[] cost, int[] dp, int n) {
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i=2; i<n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+
+        return Math.min(dp[n-1], dp[n-2]);
     }
 }
