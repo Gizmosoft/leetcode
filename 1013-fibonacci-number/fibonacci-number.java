0873aa747cc1129aad698dbce0780b5cc50a0211
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-02 15:04:54 -0400

    Time: 0 ms (100.00%) | Memory: 40.2 MB (80.50%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,12 +1,21 @@
 class Solution {
     public int fib(int n) {
-        return helper(n);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
     }
 
-    public int helper(int n) {
+    public int helper(int n, int[] dp) {
         if(n <= 0) return 0;
         if(n == 1) return 1;
 
-        return helper(n-1) + helper(n-2);
+        // Recursion
+        // return helper(n-1) + helper(n-2);
+
+        // Dynamic Programming
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = helper(n-1, dp) + helper(n-2, dp);
+        return dp[n];
     }
 }
