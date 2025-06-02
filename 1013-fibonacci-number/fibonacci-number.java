ca3331a1f214653bf74dc80a695bfa8fbd7c0082
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-02 15:34:49 -0400

    Time: 0 ms (100.00%) | Memory: 40.6 MB (39.80%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -4,10 +4,10 @@ class Solution {
         // Arrays.fill(dp, -1);     // Not required for tabulation approach
         if(n == 0) return 0;
         if(n == 1) return 1;
-        return helper(n, dp);
+        return helper(n);
     }
 
-    public int helper(int n, int[] dp) {
+    public int helper(int n) {
         // if(n <= 0) return 0;
         // if(n == 1) return 1;
 
@@ -16,15 +16,25 @@ class Solution {
 
         // Dynamic Programming
         // if(dp[n] != -1) return dp[n];
-
         // dp[n] = helper(n-1, dp) + helper(n-2, dp);
         // return dp[n];
 
-        dp[0] = 0;
-        dp[1] = 1;
+        // DP with Tabulation
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // for(int i=2; i<=n; i++) {
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+
+        // DP with Tabulation and Space Optimization
+        int prev2 = 0, prev = 1;
+
         for(int i=2; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 }
