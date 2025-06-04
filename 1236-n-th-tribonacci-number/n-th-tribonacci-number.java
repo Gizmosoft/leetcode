d437611eea64220be1d9d3918b805d2cff44b8d4
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-04 15:44:26 -0400

    Time: 0 ms (100.00%) | Memory: 40.7 MB (18.46%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -2,8 +2,12 @@ class Solution {
     public int tribonacci(int n) {
         // return recursive(n);
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return dpSol(n, dp);
+        // Arrays.fill(dp, -1);
+        // return dpSol(n, dp);
+
+        if(n == 0) return 0;
+        if(n == 2 || n == 1) return 1;
+        return dpWithTab(n, dp);
     }
 
     // RECURSION 
@@ -15,12 +19,24 @@ class Solution {
     // }
 
     // DP Solution
-    public int dpSol(int n, int[] dp) {
-        if(n <= 0) return 0;
-        if(n == 1 || n == 2) return 1;
+    // public int dpSol(int n, int[] dp) {
+    //     if(n <= 0) return 0;
+    //     if(n == 1 || n == 2) return 1;
 
-        if(dp[n] != -1) return dp[n];
-        dp[n] = dpSol(n-1, dp) + dpSol(n-2, dp) + dpSol(n-3, dp);
+    //     if(dp[n] != -1) return dp[n];
+    //     dp[n] = dpSol(n-1, dp) + dpSol(n-2, dp) + dpSol(n-3, dp);
+    //     return dp[n];
+    // }
+
+    // DP with Tabulation
+    public int dpWithTab(int n, int[] dp) {
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
         return dp[n];
     }
 }
