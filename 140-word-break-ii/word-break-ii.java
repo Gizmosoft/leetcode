0f4e68e43be2d125af35053ff61994aa18be552a
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-08 10:36:10 -0400

    Time: 7 ms (6.81%) | Memory: 42.1 MB (39.89%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,26 +1,51 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         Set<String> wordSet = new HashSet<>(wordDict);
-        List<String> results = new ArrayList<>();
-        backtrack(s, wordSet, new StringBuilder(), results, 0);
-        return results;
+        // For Dynamic Programming
+        Map<String, List<String>> dp = new HashMap<>();
+        return dfs(s, dp, wordSet);
+        // For Backtracking
+        // List<String> results = new ArrayList<>();
+        // backtrack(s, wordSet, new StringBuilder(), results, 0);
+        // return results;
     }
 
-    private void backtrack(String s, Set<String> wordSet, StringBuilder currSentence, List<String> results, int startIndex) {
-        if(startIndex == s.length()) {
-            results.add(currSentence.toString().trim());
-            return;
-        }
+    // Dynamic Programming
+    private List<String> dfs(String remainingStr, Map<String, List<String>> dp, Set<String> wordSet) {
+        if(dp.containsKey(remainingStr)) return dp.get(remainingStr);
 
-        for(int endIndex=startIndex+1; endIndex <= s.length(); endIndex++) {
-            String word = s.substring(startIndex, endIndex);
+        // base case
+        if(remainingStr.isEmpty()) return Collections.singletonList("");
+        List<String> result = new ArrayList<>();
 
-            if(wordSet.contains(word)) {
-                int currLen = currSentence.length();
-                currSentence.append(word).append(" ");
-                backtrack(s, wordSet, currSentence, results, endIndex);
-                currSentence.setLength(currLen);
+        for(int i=1; i<=remainingStr.length(); i++) {
+            String currWord = remainingStr.substring(0, i);
+            if(wordSet.contains(currWord)) {
+                for(String nextWord: dfs(remainingStr.substring(i), dp, wordSet)) {
+                    result.add(currWord + (nextWord.isEmpty() ? "" : " ") + nextWord);
+                }
             }
         }
+        dp.put(remainingStr, result);
+        return result;
     }
+
+    // BACKTRACKING
+    // private void backtrack(String s, Set<String> wordSet, StringBuilder currSentence, List<String> results, int startIndex) {
+    //     if(startIndex == s.length()) {
+    //         results.add(currSentence.toString().trim());
+    //         return;
+    //     }
+
+    //     for(int endIndex=startIndex+1; endIndex <= s.length(); endIndex++) {
+    //         String word = s.substring(startIndex, endIndex);
+
+    //         if(wordSet.contains(word)) {
+    //             int currLen = currSentence.length();
+    //             currSentence.append(word).append(" ");
+    //             backtrack(s, wordSet, currSentence, results, endIndex);
+    //             currSentence.setLength(currLen);
+    //         }
+    //     }
+    // }
 }
