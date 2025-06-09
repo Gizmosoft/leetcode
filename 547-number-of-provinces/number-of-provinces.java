ccc0b712eaffddfd8f47022e952c70cfe5139da8
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-09 11:28:42 -0400

    Time: 1 ms (88.64%) | Memory: 48.3 MB (21.68%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -7,18 +7,36 @@ class Solution {
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
                 provinces++;
-                dfs(i, isConnected, visited);
+                // dfs(i, isConnected, visited);
+                bfs(i, isConnected, visited);
             }
         }
 
         return provinces;
     }
 
-    private void dfs(int node, int[][] isConnected, boolean[] visited) {
+    // private void dfs(int node, int[][] isConnected, boolean[] visited) {
+    //     visited[node] = true;
+    //     for(int i=0; i<isConnected.length; i++) {
+    //         if(isConnected[node][i] == 1 && !visited[i]) {
+    //             dfs(i, isConnected, visited);
+    //         }
+    //     }
+    // }
+
+    private void bfs(int node, int[][] isConnected, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
         visited[node] = true;
-        for(int i=0; i<isConnected.length; i++) {
-            if(isConnected[node][i] == 1 && !visited[i]) {
-                dfs(i, isConnected, visited);
+
+        while(!q.isEmpty()) {
+            node = q.poll();
+
+            for(int i=0; i<isConnected.length; i++) {
+                if(isConnected[node][i] == 1 && !visited[i]) {
+                    q.offer(i);
+                    visited[i] = true;
+                }
             }
         }
     }
