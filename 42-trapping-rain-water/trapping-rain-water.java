3025c081d8d5f045c286d7e90b7a2e79964a5834
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-13 20:01:32 -0400

    Time: 0 ms (100.00%) | Memory: 46.9 MB (14.05%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,26 +1,51 @@
 class Solution {
     public int trap(int[] height) {
+        // BETTER APPROACH
         int n = height.length;
-        int[] prefixMax = new int[n];
-        int[] suffixMax = new int[n];
+        int leftMax = 0, rightMax = 0, total = 0, left = 0, right = n-1;
 
-        // prefix 
-        prefixMax[0] = height[0];
-        for(int i=1; i<n; i++) {
-            prefixMax[i] = Math.max(prefixMax[i-1], height[i]);
-        }
-
-        // suffix
-        suffixMax[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--) {
-            suffixMax[i] = Math.max(suffixMax[i+1], height[i]);
-        }
-
-        int total = 0;
-        for(int i=0; i<n; i++) {
-            total += Math.min(prefixMax[i], suffixMax[i]) - height[i];
+        while(left < right) {
+            if(height[left] <= height[right]) {
+                if(leftMax > height[left]) {
+                    total += leftMax - height[left];
+                } else {
+                    leftMax = height[left];
+                }
+                left += 1;
+            } else {
+                if(rightMax > height[right]) {
+                    total += rightMax - height[right];
+                } else {
+                    rightMax = height[right];
+                }
+                right -= 1;
+            }
         }
 
         return total;
+
+        // TC: O(N + N + N), SC: O(N + N)
+        // int n = height.length;
+        // int[] prefixMax = new int[n];
+        // int[] suffixMax = new int[n];
+
+        // // prefix 
+        // prefixMax[0] = height[0];
+        // for(int i=1; i<n; i++) {
+        //     prefixMax[i] = Math.max(prefixMax[i-1], height[i]);
+        // }
+
+        // // suffix
+        // suffixMax[n-1] = height[n-1];
+        // for(int i=n-2; i>=0; i--) {
+        //     suffixMax[i] = Math.max(suffixMax[i+1], height[i]);
+        // }
+
+        // int total = 0;
+        // for(int i=0; i<n; i++) {
+        //     total += Math.min(prefixMax[i], suffixMax[i]) - height[i];
+        // }
+
+        // return total;
     }
 }
