4315f0b92c22e9e7c34f05a36588e88f833c7b76
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-16 23:17:03 -0400

    Time: 1 ms (29.19%) | Memory: 44.3 MB (86.64%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -14,23 +14,35 @@
  * }
  */
 class Solution {
-    private PriorityQueue<Integer> pq;
+    // private PriorityQueue<Integer> pq;
     public int kthSmallest(TreeNode root, int k) {
-        pq = new PriorityQueue<>(Collections.reverseOrder());
-        helper(root, k, pq);
-        return pq.poll();        
+        // pq = new PriorityQueue<>(Collections.reverseOrder());
+        // helper(root, k, pq);
+        // return pq.poll();
+        List<Integer> arr = new ArrayList<>();
+        arr = inorder(root, arr);
+        return arr.get(k-1);    
     }
 
-    private void helper(TreeNode node, int k, PriorityQueue<Integer> pq) {
-        if(node == null) return;
+    private List<Integer> inorder(TreeNode node, List<Integer> arr) {
+        if(node == null) return arr;
 
-        helper(node.left, k, pq);
-        pq.add(node.val);
-        if(pq.size() > k) {
-            pq.poll();
-            return;
-        }
-        helper(node.right, k, pq);
-        return;
-    } 
+        inorder(node.left, arr);
+        arr.add(node.val);
+        inorder(node.right, arr);
+        return arr;
+    }
+
+    // private void helper(TreeNode node, int k, PriorityQueue<Integer> pq) {
+    //     if(node == null) return;
+
+    //     helper(node.left, k, pq);
+    //     pq.add(node.val);
+    //     if(pq.size() > k) {
+    //         pq.poll();
+    //         return;
+    //     }
+    //     helper(node.right, k, pq);
+    //     return;
+    // } 
 }
