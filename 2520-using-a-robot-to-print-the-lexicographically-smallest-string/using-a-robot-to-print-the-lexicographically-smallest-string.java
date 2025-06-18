af6bb3e473f9cd5d32f58cbbda28d1318e55ab04
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-18 17:52:26 -0400

    Time: 150 ms (47.64%) | Memory: 55.8 MB (7.30%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.java
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -3,21 +3,54 @@ class Solution {
         Stack<Character> t = new Stack<>();
         StringBuilder paper = new StringBuilder();
         int n = s.length();
-        char[] minSuf = new char[n];        // the smallest char in array from i'th to last position
-        minSuf[n-1] = s.charAt(n-1);
 
-        for(int i=n-2; i>=0; i--) {
-            minSuf[i] = (char) Math.min(s.charAt(i), minSuf[i+1]);
+        // Using frequency array
+        int[] freq = new int[26];
+        for(char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
 
-        for(int i=0; i<n; i++) {
-            t.push(s.charAt(i));
+        for(char ch : s.toCharArray()) {
+            t.push(ch);
+            freq[ch - 'a']--;
 
-            // while the top element of the t stack is the min in the rest of the s, pop and write to paper
-            while(!t.isEmpty() && (i == n-1 || t.peek() <= minSuf[i+1])) {
+            // check if we can pop the stack and right the character to paper
+            while(!t.isEmpty() && t.peek() <= smallestChar(freq)) {
                 paper.append(t.pop());
             }
         }
+
+        while(!t.isEmpty()) {
+            paper.append(t.pop());
+        }
+
         return paper.toString();
+
+        // Using Suffix Array
+        // char[] minSuf = new char[n];        // the smallest char in array from i'th to last position
+        // minSuf[n-1] = s.charAt(n-1);
+
+        // for(int i=n-2; i>=0; i--) {
+        //     minSuf[i] = (char) Math.min(s.charAt(i), minSuf[i+1]);
+        // }
+
+        // for(int i=0; i<n; i++) {
+        //     t.push(s.charAt(i));
+
+        //     // while the top element of the t stack is the min in the rest of the s, pop and write to paper
+        //     while(!t.isEmpty() && (i == n-1 || t.peek() <= minSuf[i+1])) {
+        //         paper.append(t.pop());
+        //     }
+        // }
+        // return paper.toString();
+    }
+
+    private char smallestChar(int[] freq) {
+        for(int i=0; i<26; i++) {
+            if(freq[i] > 0) {
+                return (char) (i + 'a');
+            }
+        }
+        return 'a';
     }
 }
