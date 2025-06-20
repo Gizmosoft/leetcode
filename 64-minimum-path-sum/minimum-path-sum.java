8a583b6d0f1267af4550a35581c73fbbb544e94b
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-20 19:46:50 -0400

    Time: 1 ms (99.73%) | Memory: 47.5 MB (69.27%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return recursive(grid, m-1, n-1, dp);
+    }   
+
+    private int recursive(int[][] grid, int i, int j, int[][] dp) {
+        if(i == 0 && j == 0) return grid[i][j];
+        if(i<0 || j<0) return 10000000;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = grid[i][j] + recursive(grid, i-1, j, dp);
+        int left = grid[i][j] + recursive(grid, i, j-1, dp);
+        dp[i][j] = Math.min(up, left);
+        return dp[i][j];
+    }
+}
