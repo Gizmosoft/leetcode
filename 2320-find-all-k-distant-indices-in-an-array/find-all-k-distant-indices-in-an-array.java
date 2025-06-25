272b1deaa392290346e9cbd9270c65b3afaa7ac0
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-06-25 17:42:40 -0400

    Time: 4 ms (66.62%) | Memory: 44.8 MB (48.89%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        List<Integer> res = new ArrayList<>();
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        boolean[] marked = new boolean[n];
         
-        for(int j=0; j<n; j++) {
-            if(nums[j] == key) {
-                int left = (j-k < 0) ? 0 : j-k;
-                int right = (j+k >=n) ? n-1 : j+k;
-
-                for(int i=left; i<=right; i++) {
-                    if(Math.abs(i-j) <= k) {
-                        if(dp[i] != -1) continue;
-                        else {
-                            dp[i] = i;
-                            res.add(i);
-                        }
-                    }
+        for (int j = 0; j < n; j++) {
+            if (nums[j] == key) {
+                int left = Math.max(0, j - k);
+                int right = Math.min(n - 1, j + k);
+                for (int i = left; i <= right; i++) {
+                    marked[i] = true;
                 }
             }
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (marked[i]) res.add(i);
         }
         return res;
     }
