0f61bee005b30967e748d4f6d01c852ba32677f5
Kartikey V Hebbar <Kartikey.hebbar@gmail.com>
2025-07-18 16:31:55 -0400

    Time: 0 ms (100.00%) | Memory: 40.8 MB (79.49%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,13 +1,20 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return checkPower(n, 0);       
+        // return checkPower(n, 0);
+        if(n == 0 || n < 0) return false;
+
+        while(n % 2 == 0) {
+            n /= 2;
+        }
+
+        return n==1;
     }
 
-    private boolean checkPower(int n, int x) {
-        if(Math.pow(2, x) == n) return true;
-        if(Math.pow(2, x) > Math.abs(n)) return false;
+    // private boolean checkPower(int n, int x) {
+    //     if(Math.pow(2, x) == n) return true;
+    //     if(Math.pow(2, x) > Math.abs(n)) return false;
 
-        boolean result = checkPower(n, x+1);
-        return result;
-    }
+    //     boolean result = checkPower(n, x+1);
+    //     return result;
+    // }
 }
